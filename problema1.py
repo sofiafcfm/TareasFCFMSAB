0e75421f8e576b64e8a0c7fab32e86c8e4254c8b
sofiafcfm <sofiapt281006@gmail.com>
2025-02-02 21:09:04 -0600

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -1,1 +1,12 @@
-print("hola mundo")
+a = int(input("INGRESE UN NUMERO")
+b = int(input("INGRESE OTRO NUMERO")
+sum=a+b
+re=a-b
+mu=a*b
+di=a/b
+dip=a//b
+res=a%b
+print(f'suma: {sum} resta: {re}  multiplicacion: {mu} division: {di} divpiso: {dip} residuo: {res}')
+
+        
+        
