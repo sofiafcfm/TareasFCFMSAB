01c2e8078b1d660f82f4ecc9b102946f5005fe93
sofiafcfm <sofiapt281006@gmail.com>
2025-02-03 20:06:16 -0600

    Create Problema9.py

---
+++ Problema9.py
@@ -0,0 +1,4 @@
+base = float(input("Ingresa la base del triángulo en metros: "))
+altura = float(input("Ingresa la altura del triángulo en metros: "))
+area = (base * altura) / 2
+print(f"El área del triángulo es: {area} metros cuadrados.")
