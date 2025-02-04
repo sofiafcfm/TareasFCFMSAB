84b0212622edc293e6751f16787409f5a65a9f0c
sofiafcfm <sofiapt281006@gmail.com>
2025-02-03 20:03:43 -0600

    Create Problema8.py

---
+++ Problema8.py
@@ -0,0 +1,5 @@
+c1= float(input("Ingrese la primer calficación"))
+c2= float(input("Ingrese la segunda calficación"))
+c3= float(input("Ingrese la tercer calficación"))
+prom=(c1+c2+c3)/3
+print(prom)
