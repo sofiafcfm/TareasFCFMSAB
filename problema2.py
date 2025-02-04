60a809d326b20a61c4fc15dd8702219f845ca31d
sofiafcfm <sofiapt281006@gmail.com>
2025-02-03 19:31:05 -0600

    Update problema2.py

--- problema2.py
+++ problema2.py
@@ -1,1 +1,10 @@
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
 
+# Determinar cuál es el mayor, menor o si son iguales
+if num1 > num2:
+    print(f"El número mayor es {num1} y el número menor es {num2}.")
+elif num1 < num2:
+    print(f"El número mayor es {num2} y el número menor es {num1}.")
+else:
+    print("Los números son iguales.")
